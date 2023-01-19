4c19ae6204f2787360efba9bc32b1a3663a5a72f
aisivan36 <aisivan36@gmail.com>
2023-01-19 22:55:18 +0700

    pushing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,14 @@ import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
 const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
-	<Stack>
+	<Stack
+		direction="row"
+		sx={{
+			overflowY: "auto",
+			height: { sx: "auto", md: "95%" },
+			flexDirection: { md: "column" },
+		}}
+	>
 		{categories.map((category) => (
 			<button>
 				<span>{category.icon}</span>
